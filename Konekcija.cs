f8c412b08e7465f705b1fc85372f8928f7622efa
Danilo Kovačević <63097315+Choda13@users.noreply.github.com>
2021-10-19 23:58:30 +0200

    Add files via upload

---
+++ Konekcija.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KontrolniA
+{
+    class Konekcija
+    {
+        static string connstr = @"Data Source=DESKTOP-KHDOA1Q; integrated security=true; Initial Catalog= Hotel";
+        static SqlConnection GetConnection()
+        {
+            return new SqlConnection(connstr);
+        }
+        public static SqlCommand GetCommand()
+        {
+            SqlCommand command = new SqlCommand();
+            command.Connection = GetConnection();
+            command.CommandType = CommandType.StoredProcedure;
+            return command;
+        }
+    }
+}
